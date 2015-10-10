aef9952d47ef43a7cb13eb74f6b996a97cc86851
Josef Friedrich <josef@friedrich.rocks>
2015-10-10 22:39:31 +0200

    Split converter in more functions.

--- jekyll-converter/converter.sh
+++ jekyll-converter/converter.sh
@@ -1,23 +1,49 @@
 #! /usr/bin/php
 <?php
 
-$server = 'localhost';
-$user   = 'august_hoegn';
-$pass   = 'xZXyM7fb8tnR9dG2';
-$dbase  = 'august_hoegn';
+function query($query) {
+	$server = 'localhost';
+	$user   = 'august_hoegn';
+	$pass   = 'xZXyM7fb8tnR9dG2';
+	$dbase  = 'august_hoegn';
 
-$conn = mysqli_connect($server, $user, $pass);
-mysqli_select_db($conn, $dbase);
+	$conn = mysqli_connect($server, $user, $pass);
+	mysqli_select_db($conn, $dbase);
 
-if ($result = mysqli_query($conn, "SELECT * FROM kompositionen;")) {
-	while ($row = mysqli_fetch_assoc($result)) {
-		$filename = $row['id'] . '.html';
-		$output = '';
-		foreach ($row as $key => $value) {
-			$output .= $key . ': ' . $value . "\n\n";
+	if ($result = mysqli_query($conn, "SELECT * FROM kompositionen;")) {
+		$rows = array();
+		while ($row = mysqli_fetch_assoc($result)) {
+			$rows[] = $row;
 		}
-		file_put_contents($filename, $output);
+		return $rows;
 	}
-	mysqli_free_result($result);
+	else {
+		return false;
+	}
 }
+
+function generate_entry($key, $value) {
+	return $key . ': ' . $value . "\n";
+}
+
+function wrap($data) {
+	return '---' . "\n" . $data . '---' . "\n";
+}
+
+function generate_document($row) {
+	$output = '';
+	foreach ($row as $key => $value) {
+		$output .= generate_entry($key, $value);
+	}
+ 	file_put_contents($row['id'] . '.html', wrap($output));
+}
+
+
+$rows = query("SELECT * FROM kompositionen;");
+
+foreach ($rows as $row) {
+	generate_document($row);
+}
+
+
 
