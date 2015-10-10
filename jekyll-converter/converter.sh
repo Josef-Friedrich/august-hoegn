4c860d71b44a93f207066c74be78be683bc23bfc
Josef Friedrich <josef@friedrich.rocks>
2015-10-10 21:25:00 +0200

    Working on jekyll converter.

--- jekyll-converter/converter.sh
+++ jekyll-converter/converter.sh
@@ -6,18 +6,18 @@ $user   = 'august_hoegn';
 $pass   = 'xZXyM7fb8tnR9dG2';
 $dbase  = 'august_hoegn';
 
-if ($conn = @mysqli_connect($server, $user, $pass)) {
-  mysqli_select_db($conn, $dbase);
-}
-else {
-  die('Die Verbindungsaufnahme zur Datenbank ist fehlgeschlagen');
+$conn = mysqli_connect($server, $user, $pass);
+mysqli_select_db($conn, $dbase);
+
+if ($result = mysqli_query($conn, "SELECT * FROM kompositionen;")) {
+	while ($row = mysqli_fetch_assoc($result)) {
+		$filename = $row['id'] . '.html';
+		$output = '';
+		foreach ($row as $key => $value) {
+			$output .= $key . ': ' . $value . "\n\n";
+		}
+		file_put_contents($filename, $output);
+	}
+	mysqli_free_result($result);
 }
 
-if ($result = mysqli_query($conn, "SELECT DATABASE()")) {
-    $row = mysqli_fetch_row($result);
-    printf("Default database is %s.\n", $row[0]);
-    mysqli_free_result($result);
-}
-
-
-print('lol');
---
+++ jekyll-converter/mysql-login.sh
@@ -0,0 +1,4 @@
+#! /bin/sh
+
+mysql --user=august_hoegn --password=xZXyM7fb8tnR9dG2 --database=august_hoegn
+
