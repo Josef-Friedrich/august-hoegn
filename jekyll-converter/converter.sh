961beeee723dea10296f4a799c141ef82aa677f0
Josef Friedrich <josef@friedrich.rocks>
2015-10-11 15:06:14 +0200

    Add compositions.

--- jekyll-converter/converter.sh
+++ jekyll-converter/converter.sh
@@ -1,11 +1,19 @@
 #! /usr/bin/php
 <?php
 
-$query = '
-SELECT
-	id,
-	titel AS title,
-	imslp
+$path = '/var/www/august-hoegn.tk/_compositions/';
+$query = 'SELECT 
+  id,
+  titel as title,
+  titeloriginal as title_original,
+  imslp as imslp_project,
+  wikicommons as wikicommons_file,
+  opus,
+  fundorttext as find_spot,
+  bes_grup as instrumentation_groups,
+  bes_grup_u_inst as instrumentation,
+  textdichter as writer,
+  liedtext as lyrics
 FROM kompositionen';
 
 function query($query) {
@@ -31,7 +39,7 @@ function query($query) {
 
 function generate_entry($key, $value) {
 	if (!empty($value)) {
-		return $key . ': ' . $value . "\n";
+		return $key . ': ' . "\"" . $value . "\"" . "\n";
 	}
 	else {
 		return '';
@@ -42,20 +50,19 @@ function wrap($data) {
 	return '---' . "\n" . $data . '---' . "\n";
 }
 
-function generate_document($row) {
+function generate_document($path, $row) {
 	$output = '';
 	foreach ($row as $key => $value) {
+		$value =  str_replace("\r\n","\n", $value);
+		$value = html_entity_decode($value, ENT_COMPAT | ENT_HTML401, 'UTF-8');
 		$output .= generate_entry($key, $value);
 	}
- 	file_put_contents('../_compositions/' . $row['id'] . '.html', wrap($output));
+ 	file_put_contents($path . $row['id'] . '.html', wrap($output));
 }
-
 
 $rows = query($query);
 
 foreach ($rows as $row) {
-	generate_document($row);
+	generate_document($path, $row);
 }
-
-
 
