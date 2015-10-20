d50ae4d88b23aa856078463842e389e24777920b
Josef Friedrich <josef@friedrich.rocks>
2015-10-20 14:26:00 +0200

    Fix documents.

--- jekyll.sh
+++ jekyll.sh
@@ -4,7 +4,7 @@ docker run --rm \
 	--label=jekyll \
 	--name jekyll_august_hoegn \
 	--volume=$(pwd):/srv/jekyll \
-	-it -p 127.0.0.1:4000:4000 \
+	-it -p 4000:4000 \
 	jekyll/jekyll:pages \
 	jekyll serve --baseurl ''
 
