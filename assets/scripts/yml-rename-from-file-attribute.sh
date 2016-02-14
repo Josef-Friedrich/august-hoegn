223c22429dff4f3b1e588db4528307f4a101a44b
Josef Friedrich <josef@friedrich.rocks>
2016-02-14 20:55:53 +0100

    Rename interviews folders

--- assets/scripts/yml-rename-from-file-attribute.sh
+++ assets/scripts/yml-rename-from-file-attribute.sh
@@ -2,11 +2,19 @@
 
 DIR='/home/jf/git-repositories/web_august-hoegn/_interviews'
 
+_get_attribute_value() {
+	cat $FILE | grep $1 | awk -F ' ' '{print $2}'
+}
+
 find $DIR -type f -print0 | while read -d $'\0' FILE ; do
 	echo $FILE
-	SHORT=$(cat $FILE | grep short: | awk -F ' ' '{print $2}')
+	ID=$(_get_attribute_value id:)
+	echo $ID
+	SHORT=$(_get_attribute_value short:)
 	echo $SHORT
 
-	mv $FILE $DIR/$SHORT.html
+	#mv $FILE $DIR/$SHORT.html
+	PROJECT_DIR=/home/jf/git-repositories/web_august-hoegn/project/exploration/interviews/files
+	mv $PROJECT_DIR/$ID $PROJECT_DIR/$SHORT
 done
 
