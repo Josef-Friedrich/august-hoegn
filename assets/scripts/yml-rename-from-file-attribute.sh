3ec7f90b90fe0fbaae7e247b0bebcbd7ceb5baa2
Josef Friedrich <josef@friedrich.rocks>
2016-02-16 22:47:00 +0100

    Add recordings to html markup

--- assets/scripts/yml-rename-from-file-attribute.sh
+++ assets/scripts/yml-rename-from-file-attribute.sh
@@ -1,20 +1,21 @@
 #! /bin/bash
 
-DIR='/home/jf/git-repositories/web_august-hoegn/_interviews'
+DIR='/home/jf/git-repositories/web_august-hoegn/_archives'
 
 _get_attribute_value() {
 	cat $FILE | grep $1 | awk -F ' ' '{print $2}'
 }
 
 find $DIR -type f -print0 | while read -d $'\0' FILE ; do
-	echo $FILE
+	DATUM=$(_get_attribute_value datum:)
 	ID=$(_get_attribute_value id:)
-	echo $ID
-	SHORT=$(_get_attribute_value short:)
-	echo $SHORT
+	echo "a${ID}a"
+	#ID=$(printf '%03d' "$ID")
+	#echo $ID
+	#echo "${DATUM}_$ID"
 
 	#mv $FILE $DIR/$SHORT.html
-	PROJECT_DIR=/home/jf/git-repositories/web_august-hoegn/project/exploration/interviews/files
-	mv $PROJECT_DIR/$ID $PROJECT_DIR/$SHORT
+	#PROJECT_DIR=/home/jf/git-repositories/web_august-hoegn/project/exploration/interviews/files
+	#mv $PROJECT_DIR/$ID $PROJECT_DIR/$SHORT
 done
 
---
+++ project/exploration/interviews/index.md
@@ -0,0 +1,13 @@
+---
+title: Interviews
+---
+
+Um mehr über den Menschen Högn zu erfahren, wurden 26 Interviews mit
+Zeitzeugen geführt. Von zwei der Tonaufnahmen wurden leider keine
+Tonaufnahmen angefertig. Alle Interviews liegen darüberhinaus in einer
+transkripierten Fassung vor und ermöglichen es dem des Niederbayerischen
+nicht mächtigen Hörer auch an diesen Gesprächen teilhaben zu können.
+
+{% include table.html collection='interviews' attributes='interviewter|location|datum' %}
+
+
